9fd6a609d32ee403ad96d8baf6b7e69c7c002708
Tarique Sultan <tarique.sultan@mmc.com>
2021-06-24 12:30:29 +0530

    fixed changes

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const apiRoutes = require('./routes/api');
 
 const app=express();
-
+    
 app.use(express.json());
 app.use(express.urlencoded({
     extended:true  
